b8692fb568f5f830ad0b58ea89f00f524252ca70
Anand Raj Singh <anandrajsingh256@gmail.com>
2022-06-08 10:36:38 +0530

    update

--- E-Commerce-Backend/src/main/java/com/masai/models/Product.java
+++ E-Commerce-Backend/src/main/java/com/masai/models/Product.java
@@ -1,25 +1,16 @@
 package com.masai.models;
 
+
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-<<<<<<< HEAD
-<<<<<<< HEAD
-import javax.validation.constraints.Pattern;
 
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-=======
-import javax.validation.constraints.Size;
->>>>>>> main
-=======
-import javax.validation.constraints.Size;
->>>>>>> main
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -30,46 +21,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer productId;
-
+	
 	@NotNull
-	@Size(min = 3, max = 30, message = "Product name size should be between 3-30")
 	private String productName;
-
 	@NotNull
-	@DecimalMin(value = "0.00")
 	private Double price;
-
 	private String description;
-
 	@NotNull
 	private String manufacturer;
-
 	@NotNull
-	@Min(value = 1)
 	private Integer quantity;
-
-	@Enumerated(EnumType.STRING)
-	private CategoryEnum category;
-
-	@Enumerated(EnumType.STRING)
-	private ProductStatus status;
-
-//	@ManyToMany(cascade = CascadeType.ALL)
-//	private Order order;
-
-//	@ManyToOne(cascade = CascadeType.ALL)
-//	private Seller seller;
+	
 
 //	@ManyToOne(cascade = CascadeType.ALL)
 //	private Cart cart;
-<<<<<<< HEAD
-<<<<<<< HEAD
 	@ManyToOne(cascade = CascadeType.ALL)
-	private Category category;
+	private CategoryEnum category;
 //	@ManyToMany(cascade = CascadeType.ALL)
 //	private Order order;
 	@ManyToOne(cascade = CascadeType.ALL)
@@ -77,9 +48,5 @@ public class Product {
 	private Seller seller;
 	
 	
-=======
->>>>>>> main
-=======
->>>>>>> main
 
 }
