8b90e27d2f19363c66611d18014fedcf4bd6f3f6
Abinash Panigrahi <abinash24x7@gmail.com>
2022-06-10 00:57:14 +0530

    delete expire tokens

--- E-Commerce-Backend/src/main/java/com/masai/models/Cart.java
+++ E-Commerce-Backend/src/main/java/com/masai/models/Cart.java
@@ -27,20 +27,7 @@ public class Cart {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer cartId;
-
-//	@JsonIgnoreProperties(allowSetters = true, value = {"quantity"})
-//	@ManyToMany
-//	@JoinTable(name = "cart_product_mapping",
-//	joinColumns = {
-//			@JoinColumn(name = "cart_id", referencedColumnName = "cartId")
-//	},
-//	inverseJoinColumns = {
-//			@JoinColumn(name = "product_id", referencedColumnName = "productId")
-//	})
-//	private List<Product> products = new ArrayList<>();
-	
-	
+	private Integer cartId;	
 	
 	@OneToMany(cascade = CascadeType.ALL)
 	private List<CartItem> cartItems = new ArrayList<>();
